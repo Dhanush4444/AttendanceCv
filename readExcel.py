e0ebae38e43ae200a7deeb95147178eb101b8a75
Dhanush4444 <dhanushdazz@gmail.com>
2019-04-13 21:07:38 +0530

    Added code to read xlsx

--- readExcel.py
+++ readExcel.py
@@ -1,1 +1,14 @@
 import xlrd
+
+loc = ('Test.xlsx')
+
+wb = xlrd.open_workbook(loc)
+sheet = wb.sheet_by_index(0)
+
+try:
+    for i in range(0, 100):
+        print(sheet.cell_value(1, i))
+except IndexError:
+    print('Index Out Of Bounds')
+
+
