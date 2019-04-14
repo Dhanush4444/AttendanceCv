4d3adcde294d711e937849d6bb332eb06eefa535
Dhanush4444 <dhanushdazz@gmail.com>
2019-04-14 15:40:25 +0530

    Added post method

--- Requests-JS/app.js
+++ Requests-JS/app.js
@@ -10,8 +10,22 @@ const request = require('request');
 //   });
 
 
-request('https://attendancesit00.herokuapp.com/api/getjson',(error, response, body)=>{
-    console.log('Error : ',error);
-    console.log('Response : ',response);
-    console.log('Body : ',body);
-});
+// request('https://attendancesit00.herokuapp.com/api/getjson',(error, response, body)=>{
+//     console.log('Error : ',error);
+//     console.log('Response : ',response);
+//     console.log('Body : ',body);
+// });
+
+
+// request('https://attendancesit00.herokuapp.com/api/update/88/25/Harish/Maths/1si15ec420', (error,response,body) => {
+//     console.log(body);
+// });
+
+
+request
+  .post('https://attendancesit00.herokuapp.com/api/update/88/25/Harish/Maths/1si15ec420')
+  .on('response', function(response) {
+    console.log(response.statusCode);
+    console.log(response.headers['content-type']);
+    // console.log(error);
+  });
