93bb6ac1a68dcd7ba84220a0919514c168de8a92
Dhanush4444 <dhanushdazz@gmail.com>
2019-04-14 15:25:34 +0530

    Added get body

--- Requests-JS/app.js
+++ Requests-JS/app.js
@@ -1,3 +1,17 @@
 'use strict';
 const request = require('request');
 
+// request
+//   .get('http://attendancesit00.herokuapp.com/api/getjson')
+//   .on('response', function(response) {
+//     console.log(response.statusCode);
+//     console.log(response.headers['content-type']);
+//     console.log(error);
+//   });
+
+
+request('https://attendancesit00.herokuapp.com/api/getjson',(error, response, body)=>{
+    console.log('Error : ',error);
+    console.log('Response : ',response);
+    console.log('Body : ',body);
+});
